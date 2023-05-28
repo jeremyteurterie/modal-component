0e8a3092464a84e5d34dafc41b6b884c6a124bad
Jeremy <jeremy.teurterie@gmail.com>
2023-05-28 15:27:18 +0700

    :sparkles: Chore: add modal stories

---
+++ src/stories/Modal.stories.jsx
@@ -0,0 +1,39 @@
+import { Modal } from '../components/Modal/Modal';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+  title: 'Example/Modal',
+  component: Modal,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Modal',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Modal',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Modal',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Modal',
+  },
+};
