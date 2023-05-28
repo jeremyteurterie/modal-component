a076f023adef08dbb7fee78fff4ac9d481e4a16e
Jeremy <jeremy.teurterie@gmail.com>
2023-05-28 16:05:11 +0700

    :building_construction: Chore: creat dist with rollup

--- src/stories/Modal.stories.jsx
+++ src/stories/Modal.stories.jsx
@@ -1,4 +1,4 @@
-import { Modal } from '../components/Modal';
+import { Modal } from '../components/Modal/Modal';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
